c65c7e42b85d69276946bd3d1a77293375f539d2
Spring8848 <tangds@hotmail.com>
2017-11-28 22:46:02 +0800

    the first commit

---
+++ src/main/java/cn/git/ecommerce/mobilepay/MobilePayApplication.java
@@ -0,0 +1,12 @@
+package cn.git.ecommerce.mobilepay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilePayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobilePayApplication.class, args);
+	}
+}
