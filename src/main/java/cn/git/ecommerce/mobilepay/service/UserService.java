ff911ed1db1d4ac8650a0f6b3437dc23aad8a374
Spring8848 <tangds@hotmail.com>
2017-11-29 12:17:54 +0800

    modify UserService mehthod

--- src/main/java/cn/git/ecommerce/mobilepay/service/UserService.java
+++ src/main/java/cn/git/ecommerce/mobilepay/service/UserService.java
@@ -2,6 +2,7 @@ package cn.git.ecommerce.mobilepay.service;
 
 public class UserService {
     public String getUserById(int id){
-        return "user id is :"+id;
+        String strId = String.valueOf(id);
+        return "user id is :"+strId;
     }
 }
